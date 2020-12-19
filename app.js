19e4db9feb2c0e768de67db211dd42a6462f2994
chetanladagi <chetan.ladagi@exathought.com>
2020-12-19 15:25:25 +0530

    second commit

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ const express = require('express');
 const app = express();
 var port = 3000;
 app.get('/', (req, res) => res.send("Hello world"));
+app.get('/test', (req, res) => res.send("Hello world"));
 app.listen(port, ()=>console.log('server is in 3000'))
