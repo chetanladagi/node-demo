4fccc963ec2f5331aaff55da2c407f8e8d330e38
chetanladagi <chetan.ladagi@exathought.com>
2020-12-19 15:49:26 +0530

    develop commit

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
+const exec = require('child_process').exec;
 app.get('/', (req, res) => res.send("Hello world1"));
 app.get('/test', (req, res) => res.send("Hello world"));
-app.listen(port, ()=>console.log('server is in 3000'))
+app.get('/test/git', (req, res) => res.send("Hello world"));
+app.get('/test/git/version', (req, res) => 
+{
+    exec(`git --version`, function(error, stdout, stderr){
+        if(error)
+            {
+                res.send({message:stderr});
+            }
+            else{
+                res.send({message:stdout});
+            }
+      });
+});
+app.listen(port, ()=>console.log('server is in 3000'));
