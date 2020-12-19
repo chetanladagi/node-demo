7ad1c1701cf385a38e4214a33a3ded3f4d98c7c1
chetanladagi <chetan.ladagi@exathought.com>
2020-12-19 18:28:46 +0530

    git clone commented commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 3000;
 const exec = require('child_process').exec;
 app.get('/', (req, res) => res.send("Hello world1"));
 app.get('/test', (req, res) => res.send("Hello world"));
-app.get('/test/git', (req, res) => res.send("Hello world"));
+app.get('/test/git', (req, res) => res.send("Hello world2"));
 app.get('/test/git/version', (req, res) => 
 {
     exec(`git --version`, function(error, stdout, stderr){
@@ -17,4 +17,18 @@ app.get('/test/git/version', (req, res) =>
             }
       });
 });
+
+app.get('/test/git/clone', (req, res) => 
+{
+    exec(`mkdir demo && cd demo && git clone https://karthik-d-n:anuradha8490@github.com/aryabot-phamax/ariya-client.git`, function(error, stdout, stderr){
+        if(error)
+            {
+                res.send({message:stderr});
+            }
+            else{
+                res.send({message:stdout});
+            }
+      });
+});
+
 app.listen(port, ()=>console.log('server is in 3000'));
